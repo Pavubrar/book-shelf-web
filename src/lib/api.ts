b0d86cfb1d6da179ced4bccb73a53b0889ac48b3
Parveen Kaur <pavubrar@gmail.com>
2026-08-24 22:34:59 +0000

    ingress impletemted at FE

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,7 +7,7 @@ import type {
   User,
 } from '../types'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5207'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? ''
 console.log("API_BASE_URL =", API_BASE_URL);
 
 type RequestOptions = {
